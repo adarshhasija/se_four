40ed7631efdba0ff628074a8c9cfc38cf2b60315
Adarsh Hasija <adarshhasija@adarshs-macbook-pro.local>
2019-03-20 22:49:04 +0530

    added ShortcutsTableViewCell

---
+++ se_vision/ShortcutsTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ShortcutsTableViewCell.swift
+//  Daykho
+//
+//  Created by Adarsh Hasija on 20/03/19.
+//  Copyright © 2019 Adarsh Hasija. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ShortcutsTableViewCell : UITableViewCell {
+    
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    
+}
