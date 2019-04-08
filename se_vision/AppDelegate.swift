92e04700ae1dec0aa67d804f8d073fbd65808301
Adarsh Hasija <hasijaadarsh@gmail.com>
2019-04-08 18:53:08 +0530

    imported Fritz

--- se_vision/AppDelegate.swift
+++ se_vision/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import Fritz
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        FritzCore.configure()
         
         return true
     }
